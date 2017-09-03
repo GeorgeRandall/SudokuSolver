3b8deca3209449c4446e2008b7cc522591accd16
George Randall <george.j.randall@gmail.com>
2017-09-03 18:56:03 -0400

    add missing copy constructor to SudokuGrid

--- SudokuSolver/SudokuGrid.cs
+++ SudokuSolver/SudokuGrid.cs
@@ -181,7 +181,15 @@ namespace SudokuSolver
 
 		public SudokuGrid(SudokuGrid toCopy)
 		{
-			//@TODO copy constructor
+			solveGrid = new gridSquare[9, 9];
+
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					solveGrid[i, j] = new gridSquare(toCopy.solveGrid[i,j]);
+				}
+			}
 		}
 
 		private gridSquare[,] solveGrid;
