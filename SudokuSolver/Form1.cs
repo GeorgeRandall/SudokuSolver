7d96cdd505fe48c898953b62132d50c573c0683e
George Randall <george.j.randall@gmail.com>
2017-09-17 14:45:16 -0400

    Add new test case for set matching

--- SudokuSolver/Form1.cs
+++ SudokuSolver/Form1.cs
@@ -897,6 +897,14 @@ namespace SudokuSolver
 				Y = new int[] { 1, 2, 4, 8, 3, 7, 2, 6, 1, 5, 6, 7, 0, 4, 8, 1, 2, 3, 7, 2, 6, 1, 5, 0, 4, 6, 7, },
 				n = new int[] { 6, 2, 3, 4, 4, 3, 9, 8, 2, 6, 5, 1, 8, 4, 6, 1, 3, 9, 8, 8, 6, 9, 7, 1, 8, 3, 5, },
 			},
+
+			//8. new full puzzle. unsolved. Should be helped by set matching
+			new testCase
+			{
+				X = new int[] { 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, },
+				Y = new int[] { 1, 3, 5, 7, 0, 2, 6, 8, 1, 7, 0, 4, 8, 3, 5, 0, 4, 8, 1, 7, 0, 2, 6, 8, 1, 3, 5, 7, },
+				n = new int[] { 4, 2, 7, 9, 9, 3, 7, 2, 1, 8, 8, 6, 3, 3, 2, 6, 1, 5, 6, 3, 3, 4, 6, 9, 5, 6, 9, 1, },
+			},
 		};
 
 		private void buttonTestCase_Click(object sender, EventArgs e)
